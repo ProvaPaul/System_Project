01d1082b31b02949c1b425d06b4fbe5609371d47
marufameem <heymarufa@gmail.com>
2025-08-01 19:09:00 +0600

    Completed the backend

--- backend/app.js
+++
@@ -1,7 +0,0 @@
-const interviewRoutes = require('./routes/interviewRoutes');
- 
-// Routes
-app.use('/api/v1', authRoutes);
-app.use('/api/v1', jobRoutes);
-app.use('/api/v1', applicationRoutes);
-app.use('/api/v1', interviewRoutes); 
--- backend/routes/interviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateInterviewQuestions, evaluateAnswers } = require('../controllers/interviewController');
-const { isAuthenticated } = require('../middleware/auth');
-
-router.post('/generate-interview-questions', isAuthenticated, generateInterviewQuestions);
-router.post('/evaluate-answers', isAuthenticated, evaluateAnswers);
-
-module.exports = router; 
